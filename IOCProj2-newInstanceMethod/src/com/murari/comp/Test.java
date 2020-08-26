0a0728eacd4b8e29beaedfce0c5f09e132ae690e
MURARI <murarikumar42@gmail.com>
2020-08-26 08:41:23 +0530

    init commit

---
+++ IOCProj2-newInstanceMethod/src/com/murari/comp/Test.java
@@ -0,0 +1,22 @@
+package com.murari.comp;
+
+public class Test {
+
+	private int a,b;
+	static {
+		System.out.println("Test.static block");
+	}
+	public Test() {
+		System.out.println("Test 0 param constructor");
+	}
+	@Override
+	public String toString() {
+		return "Test [a=" + a + ", b=" + b + "]";
+	}
+	public Test(int a, int b) {
+		System.out.println("Test::2-param constructor");
+		this.a = a;
+		this.b = b;
+	}
+	
+}
